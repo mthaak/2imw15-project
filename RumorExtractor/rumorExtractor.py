c6be3d666d61c86eee09142d48c3456db3929d85
Steven <steven_hofstede@hotmail.com>
2016-10-12 01:37:30 +0200

    Added first version of the rumor extractor component

---
+++ RumorExtractor/rumorExtractor.py
@@ -0,0 +1,77 @@
+import sklearn
+import math
+from textblob import TextBlob as tb
+import numpy as np
+
+class RumorExtractor:
+    """
+    Class that realises the Rumor Extractor component.
+    """
+
+    def __init__(self):
+        """
+        Method that initialises the Rumor Extractor component.
+        """
+        print("Initialize");
+
+    def tf(self, term, doc):
+        """
+        Method that computes the Term Frequency.
+        :param  term: the term or word.
+        :param  doc: The document or tweet containing text.
+        :return the number of times that a term occurs in document.
+        """
+        return (float)(doc.words.count(term)) / (float)(len(doc.words))
+
+    def n_containing(self, term, doclist):
+        """
+        Method that computes the number of documents containing a specific word.
+        :param  term: the term or word.
+        :param  doclist: The collection of documents or tweets.
+        :return the number of documents containing a specific term.
+        """
+        return (float)(sum(1 for blob in doclist if term in blob.words))
+
+    def idf(self, term, doclist):
+        """
+        Method that computes the Inverse Document Frequency for each term, e.g., how common or rare it is across all documents.
+        :param  term: the term or word.
+        :param  doclist: The collection of documents or tweets.
+        :return the inverse document frequency of a term on a document doc.
+        """
+        return (float)(math.log(len(doclist)) / (float)((1 + self.n_containing(term, doclist))))
+
+    def tfidf(self, term, doc, doclist):
+        """
+        Method that computes the TF-IDF.
+        :param  term: the term or word.
+        :param  doc: The document or tweet containing text.
+        :param  doclist: The collection of documents or tweets.
+        :return TF(term, doc) * IDF(term, doclist)
+        """
+        return (float)((float)(self.tf(term, doc)) * (float)(self.idf(term, doclist)))
+
+    def computeSimilarity(self, vector1, vector2):
+        """
+        Method that computes the similarity between clusters.
+        :param vector1: The first vector to compute the similarity.
+        :param vector2: The second vector to compute the similarity.
+        :return the dot-product of these vectors divided by the product of the magnitude of these vectors.
+        """
+        dot_product = sum(vector1[key] * vector2.get(key, 0) for key in vector1)
+        magnitude_v1 = math.sqrt(sum(math.pow(vector1[key], 2) for key in vector1))
+        magnitude_v2 = math.sqrt(sum(math.pow(vector2[key], 2) for key in vector2))
+        return (float)(dot_product) / ((float)(magnitude_v1) * (float)(magnitude_v2))
+
+    def mergeClusters(self, clusters, cluster1, cluster2):
+        """
+        Method that merges two clusters.
+        :param cluster1: The first cluster to merge.
+        :param cluster2: The second cluster to merge.
+        :return the new set of clusters.
+        """
+        merge = tb(cluster1.string + cluster2.string)
+        clusters.remove(cluster1)
+        clusters.remove(cluster2)
+        clusters.append(merge)
+        return clusters
---
+++ RumorExtractor/testRumor.py
@@ -0,0 +1,50 @@
+from rumorExtractor import *
+import sklearn
+import math
+from textblob import TextBlob as tb
+import nltk
+nltk.download('punkt')
+import numpy as np
+
+# Import csv file TODO: still needs to import actual csv instead of these tests
+re = RumorExtractor()
+tweet1 = tb("#zikavirus	itsleandraa	3362867932	RT @ClassicPict: Mosquitoes kill more annually than Sharks. #ZikaV")
+tweet2 = tb("#zikavirus	chatVsheil	436607416	@kanikahanda Pakaging matters however good or bad the product is & star " +
+            "performer this time is #ZikaVirus")
+tweets = [tweet1, tweet2]  # data of tweets
+threshold = 0.001 # Keep track of a threshold
+max_val = 1.0 # keep track of the maximum value in the similarity matrix. Init 1.0
+n_tweets = len(tweets)
+
+# keep clustering until threshold is reached or when there is only one cluster left.
+while max_val > threshold and n_tweets > 1:
+    tfidfs = [] #The tfidf scores of all tweets
+    simMatrix = [[0 for x in range(n_tweets)] for y in range(n_tweets)]  # Similarity matrix with size n x n tweets all set to 0.
+
+    # Compute the TF-IDF vector for each of the tweets
+    for i, tweet in enumerate(tweets):
+        print("Words in document {}".format(i + 1))
+        vector = {word: re.tfidf(word, tweet, tweets) for word in tweet.words}
+        # sorted_words = sorted(vector.items(), key=lambda x: x[1], reverse=True)
+        tfidfs.append(vector)
+        for word, score in vector.items(): # to sort in order of vector, replace with sorted_words.
+            print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
+
+    # Compute the similarity between each pair of clusters and store it in the similarity matrix.
+    for i in range(n_tweets):
+        for j in range(n_tweets):
+            if i == j:
+                simMatrix[i][j] = 0.0
+            else :
+                simMatrix[i][j] = re.computeSimilarity(tfidfs[i], tfidfs[j])
+            print("Similarity between Tweet{} and Tweet{}: {}".format(i, j, simMatrix[i][j]))
+
+    # Convert the similarity matrix into a numpy array
+    simMatrix = np.array(simMatrix)
+    # Compute the maximum value in the similarity matrix which is inside indexes 0 and 1 of index of argmax
+    tweets = re.mergeClusters(tweets, tweets[simMatrix.argmax(axis=0)[0]], tweets[simMatrix.argmax(axis=0)[1]])
+    n_tweets = len(tweets)
+    max_val = simMatrix.max()
+
+# Output the set of clusters TODO: still need to do the actual outputting of clusters
+print(tweets)
