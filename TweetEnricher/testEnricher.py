9a8b22a965c5ac3c9a7f2ed71f301057337baba8
mthaak <m.t.haak@ephi-design.com>
2016-11-04 17:20:19 +0100

    Merge branch 'master' of E:\Github\2imw15-project with conflicts.

--- TweetEnricher/testEnricher.py
+++ TweetEnricher/testEnricher.py
@@ -6,21 +6,19 @@ import datetime
 import numpy as np
 
 
-READ_FILENAME="../Data/tweets_ 20161024_111847_assertionlabeled.csv"
-WRITE_FILENAME="../Data/Features.pickle"
-WRITE_BASIC_FILENAME="../Data/Features_basic.pickle"
-WRITE_BINARY_FILENAME = "../Data/Features_binary.pickle"
-WRITE_N_GRAM_MATRIX_FILENAME = "../Data/Collection_N_Gram_Matrix.pickle"
-WRITE_SPEECH_ACT_TAG_FILENAME = "../Data/Collection_Speech_Act_Tagged.pickle"
+READ_FILENAME="../Data/tweets_20161024_111847_assertionlabeled.csv"
+WRITE_FILENAME="../Data/Features_20161024"
+WRITE_BASIC_FILENAME="../Data/Features_basic_20161024"
+WRITE_BINARY_FILENAME = "../Data/Features_binary_20161024"
+WRITE_N_GRAM_MATRIX_FILENAME = "../Data/Collection_N_Gram_Matrix_20161024"
 
 
 tweet_enricher = TweetEnricher()
 basic_enriched_tweets = [] #tweets without n gram features
 enriched_tweets = [] # tweets with n_gram features and positive,negative opinion counts
 binary_enriched_tweets = [] # tweets with all features binary
-collection=[] # stores a list of all the tweets
-collection_tweets = {} # stores all tweet ids
-collection_urls = {} # stores the tweet urls
+collection=[] # stores a document of all the tweets
+
 
 # Read tweet data from file
 with open(READ_FILENAME, encoding='utf-8') as csv_file:
@@ -29,37 +27,20 @@ with open(READ_FILENAME, encoding='utf-8') as csv_file:
         #build document for n-grmas count
         if i > 0:
             collection.append(row[1])
-            collection_tweets[row[0]] = row[1]
-            collection_urls[row[0]] = row[11]
-
-
-# speech_act_tags = tweet_enricher.speechActTagCollection(collection_tweets)
-#
-# # speech act tagged collection
-# with open(WRITE_SPEECH_ACT_TAG_FILENAME, 'wb') as out_file:
-#     pickle.dump(speech_act_tags, out_file)
-#
-# print("Collection - speech act tagged -"+ str(datetime.datetime.now().time()))
-
-#When reading pre-tagged collection
-SA_tagged_collection = pickle.load(open(WRITE_SPEECH_ACT_TAG_FILENAME, "rb"))
 
 #create n-gram count matrix and get list of features(basic and with n grams)
-basic_tweet_features,tweet_features,collection_n_gram_count_matrix = tweet_enricher.createNGramCountMatrix(collection,SA_tagged_collection)
+basic_tweet_features,tweet_features,collection_n_gram_count_matrix = tweet_enricher.createNGramCountMatrix(collection)
 
-#collection n-gram count matrix(uni,bi and tri grams)
+# collection n-gram count matrix(uni,bi and tri grams)
 with open(WRITE_N_GRAM_MATRIX_FILENAME, 'wb') as out_file:
     pickle.dump(collection_n_gram_count_matrix,out_file)
 
 print("Collection's n gram matrix generated -"+  str(datetime.datetime.now().time()))
 
-collection_n_gram_count_matrix_from_pickle = pickle.load(open(WRITE_N_GRAM_MATRIX_FILENAME, "rb"))
-
-
 #get features for each tweet in collection
-for tweet in collection_tweets:
+for tweet in collection:
     # enrich tweets and store in list
-    row_enriched_tweets, row_basic_enriched_tweets, row_binary_features = tweet_enricher.enrichTweets(collection_tweets.get(tweet),collection_urls.get(tweet))
+    row_enriched_tweets, row_basic_enriched_tweets, row_binary_features = tweet_enricher.enrichTweets(tweet)
     enriched_tweets.append(row_enriched_tweets)
     basic_enriched_tweets.append(row_basic_enriched_tweets)
     binary_enriched_tweets.append(row_binary_features)
