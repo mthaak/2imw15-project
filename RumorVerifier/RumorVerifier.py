72f156cfb6885c1b9973e08062aa50a3efceef3a
surajiyer96 <suraj.iyer96@outlook.com>
2016-11-04 14:35:50 +0100

    Updated RumorVerifier - Fixed most bugs

--- RumorVerifier/RumorVerifier.py
+++ RumorVerifier/RumorVerifier.py
@@ -5,6 +5,7 @@ import pandas as pd
 from progressbar import ProgressBar
 import math
 from dateutil import parser
+import ast
 import pickle
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.grid_search import GridSearchCV
@@ -20,29 +21,30 @@ def extract_features(file_name):
     """
     assert isinstance(file_name, str) and len(file_name) > 0
 
+    # Read tweets data
     from DataCollection.utils import read_csv_ignore_comments as read_csv
     df = read_csv(os.path.join(os.pardir, 'DataCollection', 'results', file_name))
 
-    # Load the tweet enricher
+    # initialize the tweet enricher
     tweet_enricher = TweetEnricher()
     tokens = df['text'].apply(tweet_enricher.tokenize).apply(tweet_enricher.removeStopWords)
 
-    # Load features
-    features = df['tweet_id'].to_frame()
-    print('...extracting credibility, influence, role')
-    features['credibility'] = df['verified']
-    features['influence'] = df['#followers']
-    features['role'] = df.apply(lambda x: x['#followers'] / x['#followings'])
-
+    # initialize the twitter API
+    cur_dir = os.path.curdir
+    os.chdir(os.path.join(os.path.pardir, 'DataCollection'))
     from DataCollection.twitter_api import get_tweets_of_user, search_tweets
     from DataCollection.utils import days_delta
+    os.chdir(cur_dir)
+
+    # Load features
+    features = df['tweet_id'].to_frame()
 
     # store the list of users for whom the below features is already computed
     # to prevent recomputing
     users = {}
 
     print('...extracting controversiality, originality, engagement')
-    bar = ProgressBar()
+    bar = ProgressBar()  # redirect_stdout=True)
     for index, row in bar(df.iterrows()):
         if row['screen_name'] not in users:
             col, tweets = get_tweets_of_user(row['screen_name'], nr_of_tweets=1000, save_to_csv=False)
@@ -52,19 +54,20 @@ def extract_features(file_name):
             col, replies = search_tweets('@' + row['screen_name'], nr_of_tweets=1000, save_to_csv=False)
             replies = pd.DataFrame(replies, columns=col).set_index('tweet_id')
             p_count, n_count = 0, 0
-            for i, r in tweets:
+            for i, r in tweets.iterrows():
                 replies_to_i = replies[replies['reply_to_tweet_id'] == i]
                 replies_to_i = replies_to_i['text'].apply(tweet_enricher.sentiment) \
                     .apply(lambda x: None if x[0] == x[1] else x[0] > x[1]).value_counts()
-                p_count += replies_to_i[True]
-                n_count += replies_to_i[False]
+                p_count += replies_to_i[True] if True in replies_to_i.index.values else 0
+                n_count += replies_to_i[False] if False in replies_to_i.index.values else 0
             controversiality = math.pow(p_count + n_count, min(p_count / (n_count + 1), n_count / (p_count + 1)))
             df.loc[index, 'controversiality'] = controversiality
 
             # ORIGINALITY OF USER
             is_rt = tweets['text'].apply(tweet_enricher.tokenize)
             is_rt = is_rt.apply(tweet_enricher.hasRT).apply(lambda x: x[0] == 1).value_counts()
-            originality = is_rt[False] / is_rt[True]
+            originality = (is_rt[False] if False in is_rt.index.values else 0) / \
+                          (is_rt[True] if True in is_rt.index.values else 1)
             df.loc[index, 'originality'] = originality
 
             # ENGAGEMENT OF USER
@@ -78,22 +81,45 @@ def extract_features(file_name):
             df.loc[index, 'originality'] = users[row['screen_name']][1]
             df.loc[index, 'engagement'] = users[row['screen_name']][2]
 
-    print('...extracting hasVulgarWords, hasEmoticons, isInterrogative')
+    print('...extracting credibility')
+    features['credibility'] = df['verified']
+
+    print('...extracting influence')
+    features['influence'] = df['#followers']
+
+    print('...extracting role')
+    features['role'] = df.apply(lambda x: 0 if x['#followings'] == 0 else x['#followers'] / x['#followings'], axis=1)
+
+    print('...extracting hasVulgarWords')
     features['hasVulgarWords'] = tokens.apply(tweet_enricher.hasVulgarWords)
+
+    print('...extracting hasEmoticons')
     features['hasEmoticons'] = tokens.apply(tweet_enricher.hasEmoticons)
+
+    print('...extracting isInterrogative')
     features['isInterrogative'] = tokens.apply(tweet_enricher.isInterrogative)
 
-    print('...extracting isExclamatory, hasAbbreviations, hasTwitterJargons')
+    print('...extracting isExclamatory')
     features['isExclamatory'] = tokens.apply(tweet_enricher.isExclamatory)
+
+    print('...extracting hasAbbreviations')
     features['hasAbbreviations'] = tokens.apply(tweet_enricher.hasAbbreviations)
+
+    print('...extracting hasTwitterJargons')
     features['hasTwitterJargons'] = tokens.apply(tweet_enricher.hasTwitterJargons)
 
-    print('...extracting hasFPP, hasSource, hasSpeechActVerbs')
+    print('...extracting hasFPP')
     features['hasFPP'] = tokens.apply(tweet_enricher.hasFirstPersonPronouns)
-    features['hasSource'] = df['urls'].apply(lambda x: 1 if len(x) > 0 else 0)
-    hasSpeechActVerbs = tokens.apply(tweet_enricher.hasSpeechActVerbs)
-    for key in tweet_enricher.speech_act_verbs:
-        features[key] = hasSpeechActVerbs.apply(lambda x: x[key])
+
+    print('...extracting hasSource, nr_of_sources')
+    urls = df['urls'].apply(ast.literal_eval)
+    features['hasSource'] = urls.apply(lambda x: 1 if len(x) > 0 else 0)
+    features['nr_of_sources'] = urls.apply(lambda x: len(x))
+
+    # print('...extracting hasSpeechActVerbs')
+    # hasSpeechActVerbs = tokens.apply(tweet_enricher.hasSpeechActVerbs)
+    # for key in tweet_enricher.speech_act_verbs:
+    #     features[key] = hasSpeechActVerbs.apply(lambda x: x[key])
 
     print('...extracting has#, #Position')
     has_hash = tokens.apply(tweet_enricher.hasHash)
@@ -105,14 +131,16 @@ def extract_features(file_name):
     features['hasRT'] = has_RT.apply(lambda x: x[0])
     features['RTPosition'] = has_RT.apply(lambda x: x[1])
 
-    print('...extracting has@, @Position, isRumor')
+    print('...extracting has@, @Position')
     has_a_tag = tokens.apply(tweet_enricher.hasATag)
     features['has@'] = has_a_tag.apply(lambda x: x[0])
     features['@Position'] = has_a_tag.apply(lambda x: x[1])
 
+    # print('...extracting hasNegativeOpinions, hasPositiveOpinions')
     # features['hasNegativeOpinions'] = tokens.apply(tweet_enricher.hasNegativeOpinions).apply(lambda x: 1 if x[1] else 0)
     # features['hasPositiveOpinions'] = tokens.apply(tweet_enricher.hasPositiveOpinions).apply(lambda x: 1 if x[1] else 0)
 
+    print('...creating isRumor column for manual labelling')
     features['isRumor'] = pd.Series(0, index=np.arange(features.shape[0]))
 
     # Set tweet_id as the index for the features matrix
@@ -137,12 +165,12 @@ def extract_cluster_features(file_name, features, clusters=[]):
     for index, cluster in enumerate(clusters):
         df = features.loc[cluster]
         df.to_csv(os.path.join('results', os.path.splitext(file_name)[0] + '_features_cluster_%s.csv' % index))
-        cl_df[index, 'credibility'] = df['credibility'].mean()
-        cl_df[index, 'influence'] = df['influence'].mean()
-        cl_df[index, 'role'] = df['role'].mean()
         cl_df[index, 'controversiality'] = df['controversiality'].mean()
         cl_df[index, 'originality'] = df['originality'].mean()
         cl_df[index, 'engagement'] = df['engagement'].mean()
+        cl_df[index, 'credibility'] = df['credibility'].mean()
+        cl_df[index, 'influence'] = df['influence'].mean()
+        cl_df[index, 'role'] = df['role'].mean()
         cl_df[index, 'hasVulgarWords'] = df['hasVulgarWords'].mean()
         cl_df[index, 'hasEmoticons'] = df['hasEmoticons'].mean()
         cl_df[index, 'isInterrogative'] = df['isInterrogative'].mean()
@@ -150,8 +178,10 @@ def extract_cluster_features(file_name, features, clusters=[]):
         cl_df[index, 'hasAbbreviations'] = df['hasAbbreviations'].mean()
         cl_df[index, 'hasTwitterJargons'] = df['hasTwitterJargons'].mean()
         cl_df[index, 'hasFPP'] = df['hasFPP'].mean()
-        for key in tweet_enricher.speech_act_verbs:
-            cl_df[index, key] = df[key].mean()
+        cl_df[index, 'hasSource'] = df['hasSource'].mean()
+        cl_df[index, 'nr_of_sources'] = df['nr_of_sources'].mean()
+        # for key in tweet_enricher.speech_act_verbs:
+        #     cl_df[index, key] = df[key].mean()
         cl_df[index, 'has#'] = df['has#'].mean()
         cl_df[index, '#Position'] = df['#Position'].mean()
         cl_df[index, 'hasRT'] = df['hasRT'].mean()
