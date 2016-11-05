6f757db799d00505c52df2a3b91986b1fe9c803c
Steven <steven_hofstede@hotmail.com>
2016-11-06 00:49:53 +0100

    changed filtering and final adjustment to find center and threshold tests and update

--- RumorExtractor/testRumor.py
+++ RumorExtractor/testRumor.py
@@ -4,13 +4,12 @@ import math
 from textblob import TextBlob as tb
 import nltk
 nltk.download('punkt')
-import itertools
 import numpy as np
 import csv
 
-READ_FILENAME= "../Data/tweets_brexit.csv"
 READ_TESTSET= "../Data/test.csv"
-WRITE_FILENAME="../Data/tweets_clustered.csv"
+READ_FILENAME= "../Data/tweets_20161024_111847_assertionfiltered.csv"
+WRITE_FILENAME="../Data/tweets_20161024_111847_clustered.csv"
 # Data of clusters
 clusters = []
 # Data of output tweets
@@ -18,31 +17,30 @@ init_tweets = []
 
 # Import csv file with tweets
 re = RumorExtractor()
-with open(READ_TESTSET, encoding='utf-8') as csv_file:
+with open(READ_FILENAME, encoding='utf-8') as csv_file:
     reader1 = csv.reader(csv_file, delimiter='\t')
     for i, row in enumerate(reader1):
         init_tweets.append(tb(row[0]))
 
-# Test data
-for i in range(10):
-    print(init_tweets[i].tags)
-
 # Print status report
 print("Filter Tweets")
+# Keep track of the indexes of the clusters
 index_cluster = 0
-# Tweets are filtered to only contain verbs and nouns TODO: see further what type of words need to be added / removed
+# The tags of the words that are not filtered out in the tweets
+tags = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'NN', 'NNS', 'NNP', 'NNPS', 'JJ']
+# The words that do need to be filtered out
+filter_out = ['@', 'RT', '[', ']', 'http', 'https', 'urls']
+
+# Tweets are filtered to only contain verbs and nouns
 for tweet in init_tweets:
     filtered = ""
     for i in range(len(tweet.words)):
-        try:
-            if tweet.tags[i][1] == 'VB' or tweet.tags[i][1] == 'VBD' or \
-                tweet.tags[i][1] == 'VBG' or tweet.tags[i][1] == 'VBN' or \
-                tweet.tags[i][1] == 'VBP' or tweet.tags[i][1] == 'VBZ' or \
-                tweet.tags[i][1] == 'NN' or tweet.tags[i][1] == 'NNS' or \
-                tweet.tags[i][1] == 'NNP' or tweet.tags[i][1] == 'NNPS':
+        for j in range(len(tags)):
+            try:
+                if tweet.tags[i][1] == tags[j] and not (tweet.tags[i][0] in filter_out):
                     filtered = filtered + tweet.tags[i][0] + " "
-        except:
-            print("Error")
+            except:
+                print("Error")
     # Clusters is represented as a tuple (i, j) where i is the string of merged tweets and j their indexes
     clusters.append([tb(filtered), [index_cluster]])
     index_cluster += 1
@@ -54,8 +52,8 @@ print(clusters)
 t_tfidf = []
 # TF-IDF scores of the final clusters
 c_tfidf = []
-# Keep track of a threshold TODO: look for better value of this threshold
-threshold = 0.9
+# Keep track of a threshold
+threshold = 0.8
 # keep track of the maximum value in the similarity matrix. Init 1.0
 max_val = 1.0
 # Keep track of the number of times this while loop is entered
@@ -85,14 +83,11 @@ while max_val > threshold and n_clusters > 1:
         if clustering == 1:
             t_tfidf.append(vector)
         c_tfidf.append(vector)
-        #for word, score in vector.items(): # to sort in order of vector, replace with sorted_words.
-            #print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
 
     # Compute the similarity between each pair of clusters and store it in the similarity matrix.
     for i in range(n_clusters):
         for j in range(i+1,n_clusters):
             simMatrix[i][j] = re.computeSimilarity(tfidfs[i], tfidfs[j])
-            #print("Similarity between Cluster{} and Cluster{}: {}".format(i, j, simMatrix[i][j]))
 
     # Convert the similarity matrix into a numpy array
     simMatrix = np.array(simMatrix)
@@ -105,14 +100,16 @@ while max_val > threshold and n_clusters > 1:
     n_clusters = len(clusters)
     max_val = simMatrix.max()
 
+print(clusters)
+
 # Flatten the list within lists
 for i in range(n_clusters):
     indexes = re.flatten(clusters[i][1], [])
     clusters[i] = indexes
 
-print(clusters)
 # Print status
 print("Tweets Clustered")
+print(clusters)
 print("Find centers of clusters")
 # Keep track of the centers of each cluster
 centers = []
@@ -121,12 +118,14 @@ for i in range(n_clusters):
     # Keep track of the maximum similarity
     max_sim = 0
     # Keep track of the index of the tweet that has the maximum similarity
-    i_tweet = 0
+    i_tweet = -1
     for j in range(len(clusters[i])):
         sim = re.computeSimilarity(c_tfidf[i], t_tfidf[clusters[i][j]])
         if sim > max_sim:
             max_sim = sim
             i_tweet = clusters[i][j]
+    if i_tweet == -1:
+        i_tweet = clusters[i][0]
     centers.append(i_tweet)
 
 # Output the set of clusters
@@ -134,6 +133,6 @@ for i in range(n_clusters):
 with open(WRITE_FILENAME, 'w', encoding='utf-8', newline='') as csv_file:
     writer = csv.writer(csv_file, delimiter='\t')
     for i in range(n_clusters):
-        writer.writerow(str(clusters[i]) + " ," + str(centers[i]))
+        writer.writerow(str(clusters[i]) + " ," + str([centers[i]]))
 
 print("Finished with {} clusters".format(n_clusters))
